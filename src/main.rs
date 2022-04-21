e3ad583ca40d80bddb583106428f650fe8c1e735
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-04-21 17:15:06 +0800

    change:重构上:盖上模块化

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,30 @@
 */
 
 use std::{env, fs}; //导入 读取命令行参数的模块/文件操作模块
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    //取得一段输入数据的参数值 query、filename
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        //返回一个Config结构体实例
+        Config { query, filename }
+    }
+}
+
 fn main() {
     //返回读取到的命令行参数,是一个vec的string数组
     //第一个内容是这个文件的目录，后面的内容是用户输入的参数
     //参数间用空格分隔
     let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args); //解包
 
-    let query = &args[1];
-    let filename = &args[2];
-
-    println!("Search for {}", query);
-    println!("In file {}", filename);
-
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file!");
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file!");
     println!("With text:\n{}", contents);
 }
