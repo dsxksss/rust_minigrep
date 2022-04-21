8e728897f5cd09b11a5aa6a59d38f177baf7ac80
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-04-21 17:27:27 +0800

    change:重构中:改进错误处理

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@
 
 use std::{env, fs}; //导入 读取命令行参数的模块/文件操作模块
 
+use std::process; //导入中止程序的exit函数模块
+
 struct Config {
     query: String,
     filename: String,
@@ -16,11 +18,14 @@ struct Config {
 
 impl Config {
     //取得一段输入数据的参数值 query、filename
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
         //返回一个Config结构体实例
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
 
@@ -29,7 +34,10 @@ fn main() {
     //第一个内容是这个文件的目录，后面的内容是用户输入的参数
     //参数间用空格分隔
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args); //解包
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing argunments: {}", err);
+        process::exit(1); //这里的1表示程序退出的状态码，这个可以自定义的
+    }); //这种闭包方法类似于javaScript里的箭头匿名函数一样
 
     let contents =
         fs::read_to_string(config.filename).expect("Something went wrong reading the file!");
