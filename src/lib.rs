5947224ab0df06478051b39bd5a912cc6884c723
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-04-21 18:28:10 +0800

    add:完善搜索功能,并且增加搜索测试驱动开发

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,9 @@ use std::fs; //文件操作模块
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     //?号代表不会发生panic，它会将错误值返回给调用者
     let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     Ok(())
 }
 pub struct Config {
@@ -23,5 +25,34 @@ impl Config {
         let filename = args[2].clone();
         //返回一个Config结构体实例
         Ok(Config { query, filename })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    //检查每行是否存在query这段内容
+    //如果有的话就存放到results这个数组里
+    //最后返回这个results这个集合就可以了
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        //query是测试用的搜索值
+        //contents是要进行搜索的内容
+        let query = "高性能";
+        let contents = "\
+Rust:
+高性能 - Rust 速度惊人且内存利用率极高。由于没有运行时和垃圾回收，它能够胜任对性能要求特别高的服务，可以在嵌入式设备上运行，还能轻松和其他语言集成。";
+        assert_eq!(vec!["高性能 - Rust 速度惊人且内存利用率极高。由于没有运行时和垃圾回收，它能够胜任对性能要求特别高的服务，可以在嵌入式设备上运行，还能轻松和其他语言集成。"], search(query, contents))
     }
 }
